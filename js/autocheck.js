142ebdc19310d2076f42246f1a8381d65e2c5024
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-03 01:23:08 +0300

    Update autocheck.js

---
+++ js/autocheck.js
@@ -0,0 +1,154 @@
+// 1/20
+/* const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    // Change code below this line
+    checkPizza(pizzaName) {
+      return this.pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+      const isPizzaAvailable = this.checkPizza(pizzaName);
+  
+      if (!isPizzaAvailable) {
+        return `Sorry, there is no pizza named «${pizzaName}»`;
+      }
+  
+      return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+    // Change code above this line
+  };
+  
+
+  pizzaPalace.order("Four meats") //возвращает строку "Order accepted, preparing «Four meats» pizza"
+  pizzaPalace.order("Big Mike")// возвращает строку "Sorry, there is no pizza named «Big Mike»"
+  pizzaPalace.order("Viennese") //возвращает строку "Sorry, there is no pizza named «Viennese»" */
+
+// 2/20
+/* const customer = {
+    username: "Mango",
+    balance: 24000,
+    discount: 0.1,
+    orders: ["Burger", "Pizza", "Salad"],
+    // Change code below this line
+    getBalance() {
+        return this.balance;
+    },
+    getDiscount() {
+        return this.discount;
+    },
+    setDiscount(value) {
+        this.discount = value;
+    },
+    getOrders() {
+        return this.orders;
+    },
+    addOrder(cost, order) {
+        this.balance -= cost - cost * this.discount;
+        this.orders.push(order);
+    },
+    // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"] */
+// 3//20
+/* const historyService = {
+    orders: [
+      { email: "jacob@hotmail.com", dish: "Burrito" },
+      { email: "solomon@topmail.net", dish: "Burger" },
+      { email: "artemis@coldmail.net", dish: "Pizza" },
+      { email: "solomon@topmail.net", dish: "Apple pie" },
+      { email: "jacob@hotmail.com", dish: "Taco" },
+    ],
+    // Change code below this line
+    getOrdersLog() {
+      return this.orders
+        .map(order => `email: ${order.email} dish: ${order.dish}`)
+        .join(" - ");
+    },
+    getEmails() {
+      const emails = this.orders.map(order => order.email);
+      const uniqueEmails = new Set(emails);
+      return [...uniqueEmails];
+    },
+    getOrdersByEmail(email) {
+      return this.orders.filter(order => order.email === email);
+    },
+    // Change code above this line
+  };
+   */
+// 10/20
+/* class Storage {
+    constructor([...items]) {
+        this.items = items;
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(newItem) {
+        return this.items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        return this.items.splice(1,1)
+    }
+}
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+ */
+// 11/20
+/* class StringBuilder {
+    constructor(value) {
+      this.value = value;
+    }
+    getValue() {
+        return this.value;
+      }
+    padStart(value){
+        this.value =  value + this.value 
+    }
+    padEnd(value){
+        this.value =  this.value + value ;
+    }
+    padBoth(value){
+        this.value = value + this.value + value;
+    }
+
+}
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^=" */
+
+//12/20
+class Car {
+    // Change code below this line
+  
+    constructor({ brand, model, price }) {
+      this.brand = brand;
+      this.model = model;
+      this.price = price;
+    }
+    getBrand(){
+        return this.#brand
+    }
+    changeBrand(newBrand){
+        this.#brand = newBrand
+    }
+    // Change code above this line
+  }
+  
